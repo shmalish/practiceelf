66e49c045680ce8cd6e8a29a2b57fe641f9eb918
SHMALISH <mali-rohs6@outlook.com>
2024-12-25 17:12:46 +0000

    go can now turn qr code from base64 to binary

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # docs
 
-# introduction
+## introduction
 the purpose of this project is a novel approach to malware dropping. While the implementation will not generally work in the real world, it aims to show how it can be possible to initially drop malware via QR codes. 
 
-# open source libraries used in the  project
+## open source libraries used in the  project
 Gozxing -- QR code decoder
 Segno -- QR code encoder in python which allowed me to use v40 
+
+# Project structure
+There is no meaningful project structure
--- qrdecoder.go
+++ qrdecoder.go
@@ -1,26 +1,80 @@
 package main
 
 import (
+
+	"encoding/base64"
 	"fmt"
 	"image"
 	_ "image/png"
+	"io/ioutil"
 	"os"
 
 	"github.com/makiuchi-d/gozxing"
 	"github.com/makiuchi-d/gozxing/qrcode"
 )
 
-func main() {
-	// open and decode image file
-	file, _ := os.Open("basic_qrcode_part_1.png")
-	img, _, _ := image.Decode(file)
+func decodeQRCode(filename string) (string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
 
-	// prepare BinaryBitmap
-	bmp, _ := gozxing.NewBinaryBitmapFromImage(img)
+	img, _, err := image.Decode(file)
+	if err != nil {
+		return "", err
+	}
 
-	// decode image
+	bmp, err := gozxing.NewBinaryBitmapFromImage(img)
+	if err != nil {
+		return "", err
+	}
+
+	// Decode the QR code
 	qrReader := qrcode.NewQRCodeReader()
-	result, _ := qrReader.Decode(bmp, nil)
+	result, err := qrReader.Decode(bmp, nil)
+	if err != nil {
+		return "", err
+	}
 
-	fmt.Println(result)
+	return result.GetText(), nil
+}
+
+func main() {
+	
+	decodedPart1, err := decodeQRCode("basic_qrcode_part_1.png")
+	if err != nil {
+		fmt.Println("Error decoding QR code part 1:", err)
+		return
+	}
+	fmt.Println("Decoded Part 1:", decodedPart1)
+
+	decodedPart2, err := decodeQRCode("basic_qrcode_part_2.png")
+	if err != nil {
+		fmt.Println("Error decoding QR code part 2:", err)
+		return
+	}
+	fmt.Println("Decoded Part 2:", decodedPart2)
+
+
+	combinedBase64 := decodedPart1 + decodedPart2
+
+
+	decodedBytes, err := base64.StdEncoding.DecodeString(combinedBase64)
+	if err != nil {
+		fmt.Println("Error decoding base64:", err)
+		return
+	}
+
+
+	fmt.Println("Decoded Binary Data (first 100 bytes):", decodedBytes[:100])
+
+
+	err = ioutil.WriteFile("output.bin", decodedBytes, 0644)
+	if err != nil {
+		fmt.Println("Error writing binary data to file:", err)
+		return
+	}
+
+	fmt.Println("Binary data successfully written to 'output.bin'")
 }
